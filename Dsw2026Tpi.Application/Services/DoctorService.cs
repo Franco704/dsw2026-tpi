e8446dfd030f29d9b0f6590de5cbd67dac987fbb
Ron <mateorusconi@gmail.com>
2026-07-27 14:50:53 -0300

    arreglo de validacion de matricula de doctor antes de crearlo

--- Dsw2026Tpi.Application/Services/DoctorService.cs
+++ Dsw2026Tpi.Application/Services/DoctorService.cs
@@ -6,6 +6,7 @@ using Dsw2026Tpi.CrossCutting.Helpers;
 using Dsw2026Tpi.Domain.Entities;
 using Dsw2026Tpi.Domain.Interfaces;
 using Dsw2026Tpi.CrossCutting.Exceptions;
+using System.Data;
 
 namespace Dsw2026Tpi.Application.Services;
 
@@ -20,7 +21,16 @@ public class DoctorService : IDoctorService
 
     public async Task<DoctorModel.Response> Create(DoctorModel.Request request)
     {
+        //validacion de la request
         DoctorsValidators.ValidateDoctorRequest(request);
+
+        //validacion de existencia del doctor (matricula)
+        var existing = await _persistence.First<Doctor>(d =>(d.LicenseNumber == request.LicenseNumber) && d.Deleted );
+        if (existing != null) {
+            throw new ConflictException(nameof(ErrorCodes.DOCTOR_LICENCE_CONFLICT), nameof(ErrorCodes.DOCTOR_LICENCE_CONFLICT));
+        }
+
+        //validacion de la especialidad
         var speciality = await _persistence.GetById<Speciality>(request.SpecialityId);
         if (speciality == null)
             throw new EntityNotFoundException(nameof(Speciality));
