c1e8735743e41902b25cee859867c8ce015069db
Ron <mateorusconi@gmail.com>
2026-08-01 10:21:32 -0300

    Definicion de dias no laborables, sin implementacion en availability

---
+++ Dsw2026Tpi.Domain/Entities/Feriado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Domain.Entities
+{
+    public class Feriado
+    {
+        public DateTime Fecha { get; set; }
+        public string Dia { get; set; } = "";
+        public string Motivo { get; set; } = "";
+        public string Tipo { get; set; } = "";
+    }
+
+}
