61bcaa7ddfe8478b6a077485864531f118d9b858
Ron <mateorusconi@gmail.com>
2026-08-03 22:57:45 -0300

    politicas del modulo doctores fixeadas

--- Dsw2026Tpi.Api/Controllers/DoctorController.cs
+++ Dsw2026Tpi.Api/Controllers/DoctorController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Dsw2026Tpi.Api.Controllers;
 
 [Route("api/doctors")]
-[Authorize(Policy = Policies.AdminPolicy)]
+[Authorize]
 public class DoctorController : AppController
 {
     private readonly IDoctorService _service;
@@ -18,6 +18,7 @@ public class DoctorController : AppController
     }
 
     [HttpGet]
+    [Authorize(Policy = Policies.AdminPolicy)]  
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<IActionResult> GetAll([FromQuery] int pageSize, [FromQuery] int pageIndex, [FromQuery] string? name = null)
     {
@@ -35,6 +36,7 @@ public class DoctorController : AppController
     }
 
     [HttpPost]
+    [Authorize(Policy = Policies.AdminPolicy)]
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -45,6 +47,7 @@ public class DoctorController : AppController
     }
 
     [HttpPut("{id}")]
+    [Authorize(Policy = Policies.AdminPolicy)]
     [ProducesResponseType(
     typeof(DoctorModel.Response),
     StatusCodes.Status200OK)]
@@ -65,6 +68,7 @@ public class DoctorController : AppController
 
 
     [HttpDelete("{id}")]
+    [Authorize(Policy = Policies.AdminPolicy)]
     [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> DeleteDoctor(
